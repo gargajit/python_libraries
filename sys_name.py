965ce35ee9507dedd009b798d6685711e7b51a52
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-01-05 00:14:43 +0530

    Update sys_name.py

--- sys_name.py
+++ sys_name.py
@@ -1,6 +1,29 @@
 import sys
 
+'''
 try:
     print("Hello, my name is", sys.argv[1])  #at [0] is the script name eg: name.py and [1] is command-line argument
 except IndexError:
     print("script name:", sys.argv[0])
+'''
+
+#----------------------------------------------
+
+'''
+# Check for errors
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+
+# print name tags
+print("Hello, my name is", sys.argv[1])
+'''
+
+#-----------------------------------------------
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+for arg in sys.argv[1:]:
+    print("Hello, My name is", arg)
