d4184d6329aa734f1c9d22c64b6180f65708a0f3
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-01-04 23:52:57 +0530

    Update sys_name.py

--- sys_name.py
+++ sys_name.py
@@ -1,5 +1,6 @@
 import sys
 
-print("Hello, my name is", sys.argv[1])  #at [0] is the script name eg: name.py and [1] is command-line argument
-
-print("script name:", sys.argv[0])
+try:
+    print("Hello, my name is", sys.argv[1])  #at [0] is the script name eg: name.py and [1] is command-line argument
+except IndexError:
+    print("script name:", sys.argv[0])
